b3bf66b54efdc0364337f0176756ee57c35aa6e6
Shariqbinrashid <shariqbinrashid@gmail.com>
2020-12-25 22:00:28 +0500

    router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home(props){
+    return(
+
+        <div className="Container">
+            <h4>
+                Home
+            </h4>
+        </div>
+    );
+}
+
+export default Home;
